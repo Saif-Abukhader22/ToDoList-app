f63bc443b9a11bf29b6a9df2c8fca5c55b18f267
saifabukhader22 <saif@kenosisgroup.com>
2025-08-17 10:49:15 +0300

    deploying test 1

--- backEnd/database.py
+++ backEnd/database.py
@@ -2,17 +2,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import os
 
 #sessionmaker helps create a session — a temporary connection to run queries.
 #eclarative_base is used to define models (tables) using Python classes.
 
-DATABASE_URL = "postgresql://postgres:123456@localhost/todos_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL) #create connection with DB
+
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql+psycopg2://postgres:123456@localhost/todos_db"
+
+    print("🚀 Using database:", DATABASE_URL)
+
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True) #create connection with DB
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False) # This builds a factory to create new Session objects.
 
 Base = declarative_base()
 
-
+a

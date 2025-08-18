3034995d94e0ca6072a7670fd6be9943591ee3f9
saifabukhader22 <saif@kenosisgroup.com>
2025-08-18 11:17:47 +0300

    deploying test 5.9.7

--- my-todo-frontend/src/api.ts
+++ my-todo-frontend/src/api.ts
@@ -1,10 +1,11 @@
-import axios, { AxiosHeaders, type InternalAxiosRequestConfig} from "axios";
-
+import axios, { AxiosHeaders, type InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
-
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem("token");
@@ -12,7 +13,6 @@ api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const isAuthCall = url.startsWith("/auth/");
 
   if (token && !isAuthCall) {
-    // ensure we have an AxiosHeaders instance
     const headers = config.headers instanceof AxiosHeaders
       ? config.headers
       : new AxiosHeaders(config.headers);
@@ -20,8 +20,6 @@ api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     headers.set("Authorization", `Bearer ${token}`);
     config.headers = headers;
   }
-  console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
-
 
   return config;
 });
